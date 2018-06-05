0880f2e1a0937c10b6bd138f16344c59e72438fa
uittorio <vittorio.gue@gmail.com>
2018-06-05 10:29:52 +0200

    remove command line because not necessary

--- fileTemplateGenerator/fileTemplateGenerator.js
+++ fileTemplateGenerator/fileTemplateGenerator.js
@@ -1,9 +1,11 @@
 const Generator = require('../lib/generator/generator');
+const ProcessService = require('../lib/process/process');
+const processService = new ProcessService(process);
 
 class FileTemplateGenerator {
   static generate(config) {
     config.files.forEach((file) => {
-      new Generator(file, config.path, config.parameters).createFile();
+      new Generator(file, config.path || processService.getFolder(), config.parameters).createFile();
     });
   }
 }
